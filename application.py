3902e35cca5947c381affaf83194b69ccd8e9ffb
Hans Trinkle <htrinkle@telstra.com>
2020-01-28 12:05:06 +1100

    Created web api structure

--- application.py
+++ application.py
@@ -1,10 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, Blueprint, render_template
+from flask_restplus import Api, Resource
 
-@app.route("/")
-def hello():
-    return "Hello from hans"
+app = Flask(
+  __name__, 
+  static_url_path = "/static", 
+  static_folder = "static",
+  template_folder = "templates"
+)
 
-@app.route("/api")
-def api():
-    return "API"
+########## API 
+
+bp = Blueprint(
+  'api',
+  __name__,
+)
+
+api = Api(app=bp)
+
+class Sensor(Resource):
+  def get(self):
+    return "GET Method on sensor"
+  def post(self):
+    return "POST Method on sensor"
+
+api.add_resource(Sensor, '/sensor')
+app.register_blueprint(bp, url_prefix='/api')
+
+########## Web Page 
+
+@app.route('/')
+def root():
+  return render_template('root.html')
+
+
+
+
