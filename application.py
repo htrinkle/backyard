480b05d7d61eae8a245da52c4d19d0e858b8bf64
Hans Trinkle <htrinkle@telstra.com>
2020-07-22 10:59:27 +1000

    from makefile

--- application.py
+++ application.py
@@ -1,64 +1,20 @@
 from flask import Flask, Blueprint, render_template
-from flask_restplus import Api, Resource
-# from webargs import fields, validate
-# from webargs.flaskparser import use_kwargs, use_args, parser
-
-import datetime as dt
+from flask_restx import Api, Resource
 
 app = Flask(
-  __name__, 
-  static_url_path = "/static", 
-  static_folder = "static",
-  template_folder = "templates"
-)
+    __name__ )
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint, doc='/doc/')
+app.register_blueprint(blueprint)
 
-########## API 
+@api.route('/<device>/<metric>')
+@api.doc(params={'device': 'device name', 'metric': 'sensor topic'})
+class Device(Resource):
+  def get(self, device, metric):
+    return {}
 
-bp = Blueprint(
-  'api',
-  __name__,
-)
-
-api = Api(app=bp)
-
-readings = {
-      '2020-01-02T16:34:21':{
-        'temp' : 235,
-        'humidity' : 850
-      },
-      '2020-01-02T17:34:21':{
-        'temp' : 135,
-        'humidity' : 550
-      }
-    }
-
-
-class Sensor(Resource):
-  #@api.expect(sensor_arg, validate=True)
-  def put(self, id):
-    """ Install a new sensor """
-    return {"sensor":"added"}, 201 
-  
-
-class Measurement(Resource):
-  #@api.expect(measurement_arg, validate=True)
-  def put(self, id):
-    """ Return list of all sensor readings. """
-    return {"measurement":"added"}, 201 
-
-class History(Resource):
-  #@api.expect(history_arg, validate=True)
-  def get(self):
-    """ Return filtered list of measurements """
-    return {
-      'happy':'now'
-    }, 200
-
-api.add_resource(Measurement, '/sensor/<id>/measurement')
-api.add_resource(Sensor, '/sensor/<id>')
-api.add_resource(History, '/history')
-
-app.register_blueprint(bp, url_prefix='/api')
+  def post(self, device, metric):
+    print("Device: " + device);
 
 ########## Web Page 
 
@@ -68,6 +24,7 @@ def root():
 
 if __name__ == '__main__':
   app.run(debug=True)
+
 
 
 
