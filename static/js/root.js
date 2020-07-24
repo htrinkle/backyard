a9760dfd6256e975b67ff41d62a48b98c8e2df4e
Hans Trinkle <htrinkle@telstra.com>
2020-07-24 14:40:37 +1000

    from makefile

--- static/js/root.js
+++ static/js/root.js
@@ -1,11 +1,10 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'scatter',
 
     // The data for our dataset
     data: {
-        labels: [],
         datasets: [{
             label: 'Temperature',
             backgroundColor: 'rgb(255, 99, 132)',
@@ -15,18 +14,33 @@ var chart = new Chart(ctx, {
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+      legend: false,
+      scales: {
+        xAxes: [{
+          ticks: {
+            callback: function(value, index, values) {
+              return moment(value*1000).format('DD:MM:YYYY h:mm a')
+            }
+          }
+        }],
+      }
+    }
 });
 
+var gaugeTest = document.getElementById('gaugeText')
 var gauge = new LinearGauge({
 	renderTo: 'myGauge',
 	width: 100,
+    height: 400,
 	units: "°C",
 	value: 0
 }).draw();
 
+var n = 100
+
 function fetchData() {
-	fetch('https://backyard.azurewebsites.net/api/hansnodemcu?key=1234abcd')
+	fetch('https://backyard.azurewebsites.net/api/hansnodemcu?key=1234abcd&n='+n.toString())
 		.then(response => response.json())
 		.then(data => updateData(data));
 }
@@ -39,16 +53,27 @@ function updateData(data) {
 	
 	console.log(t_list)
 	console.log(v_list)
-    console.log(t_list[t_list.length - 1])	
-	
-	chart.data['labels'] = t_list
-	chart.data['datasets'][0]['data'] = v_list
+
+    var data = []
+    for (i=0; i<v_list.length; i++) {
+      data.push({'x':t_list[i], 'y':v_list[i]})
+    }
+
+	chart.data['datasets'][0]['data'] = data
 	chart.update();
 	
 	gauge.value = v_list[v_list.length - 1]
+    gaugeText.innerHTML = "Temperature recorded at " + 
+      moment(t_list[t_list.length -1]*1000).format('DD:MM:YYYY h:mm:ss a')
 	gauge.update()
 	
 }		
+
+function setN() {
+  n=document.getElementById("numSamples").value
+  console.log(n);
+  fetchData();
+}
 
 var intervalId = setInterval(fetchData, 10000);
 fetchData();
